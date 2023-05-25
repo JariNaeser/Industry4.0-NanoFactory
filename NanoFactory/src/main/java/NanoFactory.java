bce111b287711929dc27731db2e5cc7b2c7b9262
Jari Näser <jari.naeser@supsi.ch>
2023-05-25 16:04:32 +0200

    Implemented operator input

--- NanoFactory/src/main/java/NanoFactory.java
+++ NanoFactory/src/main/java/NanoFactory.java
@@ -1,12 +1,17 @@
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
 import com.influxdb.client.domain.WritePrecision;
+
+import java.io.IOException;
 import java.time.Instant;
 import com.influxdb.client.write.Point;
 import org.iot.raspberry.grovepi.GrovePi;
 import org.iot.raspberry.grovepi.pi4j.GrovePi4J;
 import org.iot.raspberry.grovepi.sensors.analog.GroveRotarySensor;
+import org.iot.raspberry.grovepi.sensors.digital.GroveButton;
 import org.iot.raspberry.grovepi.sensors.i2c.GroveRgbLcd;
+import org.iot.raspberry.grovepi.sensors.listener.GroveButtonListener;
+import org.iot.raspberry.grovepi.sensors.synch.SensorMonitor;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +27,7 @@ public class NanoFactory {
     /* -------------------------- CONSTANTS -------------------------- */
     public static final String SORTER_NAME = "Sorter";
     public static final String TRAPDOOR_NAME = "Trapdoor";
+    public static final String HUMAN_INTERVENTION = "HumanIntervention";
     public static final int RS_THRESHOLD = 10;
     public static final int TRAPDOOR_BRIGHT_OPEN = 47;    // Based on blue opening, degrees
     public static final int TRAPDOOR_DARK_OPEN = 157;     // Based on blue opening, degrees
@@ -33,6 +39,8 @@ public class NanoFactory {
 
     private static SorterStatus registerSorterEvent;
     private static TrapdoorStatus lastTrapdoorStatus;
+    private static int buttonClickCounter = 0;
+    public static InfluxDBClient client;
 
     /* -------------------------- ENUMS -------------------------- */
 
@@ -69,15 +77,36 @@ public class NanoFactory {
     /*
         This method saves a point on the Influx Database
      */
-    private static void saveMeasurementOnInflux(String sourceSensorName, String sourceSensorStatus, double sourceSensorValue, InfluxDBClient client){
-        Point point = Point
-                .measurement(sourceSensorName)                       // Nome sensore
-                .addTag("SensorStatus", sourceSensorStatus)          // Stato sensore
-                .addField("SensorValue", sourceSensorValue)     // Valore sensore
-                .time(Instant.now(), WritePrecision.NS);
+    private static void saveMeasurementOnInflux(String sourceSensorName, String sourceSensorStatus, double sourceSensorValue){
+        if(client != null){
+            Point point = Point
+                    .measurement(sourceSensorName)                       // Nome sensore
+                    .addTag("SensorStatus", sourceSensorStatus)          // Stato sensore
+                    .addField("SensorValue", sourceSensorValue)     // Valore sensore
+                    .time(Instant.now(), WritePrecision.NS);
 
-        // Write on influx DB
-        client.getWriteApiBlocking().writePoint(INFLUX_BUCKET, INFLUX_ORG, point);
+            // Write on influx DB
+            client.getWriteApiBlocking().writePoint(INFLUX_BUCKET, INFLUX_ORG, point);
+        }
+    }
+
+    private static void updateLCDScreen(GroveRgbLcd lcdScreen){
+        try{
+            lcdScreen.setRGB(255,255,255);
+            lcdScreen.setText(String.format("Errori: %d", buttonClickCounter));
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
+    private static void giveGreenFedback(GroveRgbLcd lcdScreen){
+        try{
+            lcdScreen.setRGB(0,255,0);
+            Thread.sleep(50);
+            lcdScreen.setRGB(255,255,255);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -85,18 +114,78 @@ public class NanoFactory {
         Logger.getLogger("GrovePi").setLevel(Level.WARNING);
         Logger.getLogger("RaspberryPi").setLevel(Level.WARNING);
         GrovePi grovePi = new GrovePi4J();
+
         // Influx setup
-        InfluxDBClient client = InfluxDBClientFactory.create(INFLUX_CONNECTION_URL, INFLUX_TOKEN.toCharArray());
+        client = InfluxDBClientFactory.create(INFLUX_CONNECTION_URL, INFLUX_TOKEN.toCharArray());
 
         // Components setup
-        GroveRotarySensor trapdoorRotatorySensor = new GroveRotarySensor(grovePi, 1);   // Analogic
-        GroveRotarySensor sorterRotatorySensor = new GroveRotarySensor(grovePi, 0);     // Analogic
-        GroveRotarySensor operatorRotatorySensor = new GroveRotarySensor(grovePi, 2);   // Analogic
-
         GroveRgbLcd lcdScreen = grovePi.getLCD();
 
-        lcdScreen.setRGB(255,255,255);
-        lcdScreen.setText("Hello");
+        GroveRotarySensor trapdoorRotatorySensor = new GroveRotarySensor(grovePi, 1);   // Analogic
+        GroveRotarySensor sorterRotatorySensor = new GroveRotarySensor(grovePi, 0);     // Analogic
+
+        GroveButton buttonIncrement = new GroveButton(grovePi, 7);
+        GroveButton buttonDecrement = new GroveButton(grovePi, 8);
+        GroveButton buttonSubmit = new GroveButton(grovePi, 6);
+
+        SensorMonitor<Boolean> buttonIncrementSensorMonitor = new SensorMonitor<>(buttonIncrement, 100);
+        SensorMonitor<Boolean> buttonDecrementSensorMonitor = new SensorMonitor<>(buttonDecrement, 100);
+        SensorMonitor<Boolean> buttonSubmitSensorMonitor = new SensorMonitor<>(buttonSubmit, 100);
+
+        buttonIncrement.setButtonListener(new GroveButtonListener() {
+            @Override
+            public void onRelease() {}
+            @Override
+            public void onPress() {}
+            @Override
+            public void onClick() {
+                buttonClickCounter++;
+                updateLCDScreen(lcdScreen);
+            }
+        });
+
+        buttonDecrement.setButtonListener(new GroveButtonListener() {
+            @Override
+            public void onRelease() {}
+            @Override
+            public void onPress() {}
+
+            @Override
+            public void onClick() {
+                if(buttonClickCounter > 0){
+                    buttonClickCounter--;
+                    updateLCDScreen(lcdScreen);
+                }
+            }
+        });
+
+        buttonSubmit.setButtonListener(new GroveButtonListener() {
+            @Override
+            public void onRelease() {}
+            @Override
+            public void onPress() {}
+
+            @Override
+            public void onClick() {
+                saveMeasurementOnInflux(
+                        HUMAN_INTERVENTION,
+                        HUMAN_INTERVENTION,
+                        buttonClickCounter
+                );
+
+                buttonClickCounter = 0;
+                updateLCDScreen(lcdScreen);
+
+                // Give green feedback
+                giveGreenFedback(lcdScreen);
+            }
+        });
+
+        buttonIncrementSensorMonitor.start();
+        buttonDecrementSensorMonitor.start();
+        buttonSubmitSensorMonitor.start();
+
+        updateLCDScreen(lcdScreen);
 
         while(true){
             // Sorter
@@ -106,8 +195,7 @@ public class NanoFactory {
                     saveMeasurementOnInflux(
                             SORTER_NAME,
                             SorterStatus.IDLE.description,
-                            sorterRotatorySensor.get().getDegrees(),
-                            client
+                            sorterRotatorySensor.get().getDegrees()
                     );
 
                     registerSorterEvent = SorterStatus.IDLE;
@@ -118,8 +206,7 @@ public class NanoFactory {
                     saveMeasurementOnInflux(
                             SORTER_NAME,
                             SorterStatus.BRIGHT_SIDE.description,
-                            sorterRotatorySensor.get().getDegrees(),
-                            client
+                            sorterRotatorySensor.get().getDegrees()
                     );
 
                     registerSorterEvent = SorterStatus.BRIGHT_SIDE;
@@ -130,8 +217,7 @@ public class NanoFactory {
                     saveMeasurementOnInflux(
                             SORTER_NAME,
                             SorterStatus.DARK_SIDE.description,
-                            sorterRotatorySensor.get().getDegrees(),
-                            client
+                            sorterRotatorySensor.get().getDegrees()
                     );
 
                     registerSorterEvent = SorterStatus.DARK_SIDE;
@@ -145,8 +231,7 @@ public class NanoFactory {
                     saveMeasurementOnInflux(
                             TRAPDOOR_NAME,
                             TrapdoorStatus.BRIGHT_OPEN.description,
-                            trapdoorRotatorySensor.get().getDegrees(),
-                            client
+                            trapdoorRotatorySensor.get().getDegrees()
                     );
 
                     lastTrapdoorStatus = TrapdoorStatus.BRIGHT_OPEN;
@@ -157,8 +242,7 @@ public class NanoFactory {
                     saveMeasurementOnInflux(
                             TRAPDOOR_NAME,
                             TrapdoorStatus.DARK_OPEN.description,
-                            trapdoorRotatorySensor.get().getDegrees(),
-                            client
+                            trapdoorRotatorySensor.get().getDegrees()
                     );
 
                     lastTrapdoorStatus = TrapdoorStatus.DARK_OPEN;
